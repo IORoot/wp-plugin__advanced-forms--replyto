32123c0a4ba6ce5b43e5336684ae21c38c44d6a0
Andy Pearson <andy@londonparkour.com>
2020-05-25 15:29:51 +0100

    Better Description and Readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advanced Forms Pro - Reply-to header
+
+Add a filter to automatically add the 'reply-to' header in any emails sent.
+This means that the reply-to is to the author of the email, NOT the server address
+of londonparkour.com.
--- advanced_forms_replyto.php
+++ advanced_forms_replyto.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: _ANDYP - Advanced Forms Pro - Add Reply-to Header
 Plugin URI: https://londonparkour.com
-Description: Emails from Advanced Forms Pro do not have the reply-to header by default.
+Description: <strong>FILTER</strong> | Emails from Advanced Forms Pro do not have the reply-to header by default.
 Version: 1.0.0
 Author: Andy Pearson
 Author URI: https://londonparkour.com
